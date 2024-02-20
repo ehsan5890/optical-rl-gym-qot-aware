e953655f26d77689743c8909778d46ba67fc6020
ehsan5890 <ee.etezadi@gmail.com>
2024-02-20 14:14:28 +0100

    first FRA RMSA is developed

--- tests/test_rmsa.py
+++ tests/test_rmsa.py
@@ -5,21 +5,23 @@ import pickle
 import gym
 import matplotlib.pyplot as plt
 import numpy as np
-
-from optical_rl_gym.envs.rmsa_env import (
-    SimpleMatrixObservation,
-    least_loaded_path_first_fit,
+from scipy.io import loadmat
+from optical_rl_gym.envs.phy_rmsa_env import (
     shortest_available_path_first_fit,
-    shortest_path_first_fit,
+    phy_aware_sapff_rmsa,
+    phy_aware_bmff_rmsa,
+    phy_aware_bmfa_rmsa,
+    phy_aware_bmfa_rss_rmsa
+
 )
 from optical_rl_gym.utils import evaluate_heuristic, random_policy
 
-load = 50
-logging.getLogger("rmsaenv").setLevel(logging.INFO)
+load = 1300
+# logging.getLogger("rmsaenv").setLevel(logging.INFO)
 
 seed = 20
-episodes = 10
-episode_length = 100
+episodes = 1
+episode_length = 100000
 
 monitor_files = []
 policies = []
@@ -27,10 +29,21 @@ policies = []
 # topology_name = 'gbn'
 # topology_name = 'nobel-us'
 # topology_name = 'germany50'
+topology_name='jpn12'
+# topology_name='nsfnet_chen'
 with open(
-    os.path.join("..", "examples", "topologies", "nsfnet_chen_5-paths_6-modulations.h5"), "rb"
+    os.path.join("..", "examples", "topologies", f"{topology_name}_5-paths_6-modulations.h5"), "rb"
 ) as f:
     topology = pickle.load(f)
+
+mat_file = loadmat('../examples/inputs/Modulation_connection_JPN12_k7SP_CHBFullyLoaded_SCL_Uniform.mat')
+mat_file1 = loadmat('../examples/inputs/GSNR_connection_JPN12_k7SP_CHBFullyLoaded_SCL_Uniform.mat')
+mat_file2 = loadmat('../examples/inputs/All_connections_Profile_JPN12_k7SP_CHBFullyLoaded_SCL_Uniform.mat')
+
+modulation_jpn12 = mat_file['Modulation_connection_JPN12_k7SP_CHBFullyLoaded_SCL_Uniform']
+gsnr_jpn12 = mat_file1['GSNR_connection_JPN12_k7SP_CHBFullyLoaded_SCL_Uniform']
+all_connections_jpn12 = mat_file2['All_connections_Profile_JPN12_k7SP_CHBFullyLoaded_SCL_Uniform']
+
 
 env_args = dict(
     topology=topology,
@@ -41,73 +54,78 @@ env_args = dict(
     episode_length=episode_length,
     num_spectrum_resources=64,
     bit_rate_selection="discrete",
+    modulation_level=modulation_jpn12,
+    connections_detail=all_connections_jpn12,
+    gsnr=gsnr_jpn12,
+    number_spectrum_channels=80,
+    number_spectrum_channels_s_band=108,
 )
 
 print("STR".ljust(5), "REW".rjust(7), "STD".rjust(7))
 
-init_env = gym.make("RMSA-v0", **env_args)
-env_rnd = SimpleMatrixObservation(init_env)
-mean_reward_rnd, std_reward_rnd = evaluate_heuristic(
-    env_rnd, random_policy, n_eval_episodes=episodes
-)
-print("Rnd:".ljust(8), f"{mean_reward_rnd:.4f}  {std_reward_rnd:>7.4f}")
-print(
-    "\tBit rate blocking:",
-    (init_env.episode_bit_rate_requested - init_env.episode_bit_rate_provisioned)
-    / init_env.episode_bit_rate_requested,
-)
-print(
-    "\tRequest blocking:",
-    (init_env.episode_services_processed - init_env.episode_services_accepted)
-    / init_env.episode_services_processed,
-)
-print(init_env.topology.graph["throughput"])
-# exit(0)
 
-env_sp = gym.make("RMSA-v0", **env_args)
+env_phy_df = gym.make("PhyRMSA-v0", **env_args)
 mean_reward_sp, std_reward_sp = evaluate_heuristic(
-    env_sp, shortest_path_first_fit, n_eval_episodes=episodes
+    env_phy_df, phy_aware_sapff_rmsa, n_eval_episodes=episodes
 )
-print("SP-FF:".ljust(8), f"{mean_reward_sp:.4f}  {std_reward_sp:<7.4f}")
+print("SAP-FF:".ljust(8), f"{mean_reward_sp:.4f}  {std_reward_sp:<7.4f}")
 print(
     "\tBit rate blocking:",
-    (env_sp.episode_bit_rate_requested - env_sp.episode_bit_rate_provisioned)
-    / env_sp.episode_bit_rate_requested,
+    (env_phy_df.episode_bit_rate_requested - env_phy_df.episode_bit_rate_provisioned)
+    / env_phy_df.episode_bit_rate_requested,
 )
 print(
     "\tRequest blocking:",
-    (env_sp.episode_services_processed - env_sp.episode_services_accepted)
-    / env_sp.episode_services_processed,
+    (env_phy_df.episode_services_processed - env_phy_df.episode_services_accepted)
+    / env_phy_df.episode_services_processed,
 )
 
-env_sap = gym.make("RMSA-v0", **env_args)
-mean_reward_sap, std_reward_sap = evaluate_heuristic(
-    env_sap, shortest_available_path_first_fit, n_eval_episodes=episodes
+env_phy_bmff_df = gym.make("PhyRMSA-v0", **env_args)
+mean_reward_sp, std_reward_sp = evaluate_heuristic(
+    env_phy_bmff_df, phy_aware_bmff_rmsa, n_eval_episodes=episodes
 )
-print("SAP-FF:".ljust(8), f"{mean_reward_sap:.4f}  {std_reward_sap:.4f}")
+print("BM-FF:".ljust(8), f"{mean_reward_sp:.4f}  {std_reward_sp:<7.4f}")
 print(
     "\tBit rate blocking:",
-    (env_sap.episode_bit_rate_requested - env_sap.episode_bit_rate_provisioned)
-    / env_sap.episode_bit_rate_requested,
+    (env_phy_bmff_df.episode_bit_rate_requested - env_phy_bmff_df.episode_bit_rate_provisioned)
+    / env_phy_bmff_df.episode_bit_rate_requested,
 )
 print(
     "\tRequest blocking:",
-    (env_sap.episode_services_processed - env_sap.episode_services_accepted)
-    / env_sap.episode_services_processed,
+    (env_phy_bmff_df.episode_services_processed - env_phy_bmff_df.episode_services_accepted)
+    / env_phy_bmff_df.episode_services_processed,
 )
 
-env_llp = gym.make("RMSA-v0", **env_args)
-mean_reward_llp, std_reward_llp = evaluate_heuristic(
-    env_llp, least_loaded_path_first_fit, n_eval_episodes=episodes
+
+env_phy_bmfa_df = gym.make("PhyRMSA-v0", **env_args)
+mean_reward_sp, std_reward_sp = evaluate_heuristic(
+    env_phy_bmfa_df, phy_aware_bmfa_rmsa, n_eval_episodes=episodes
 )
-print("LLP-FF:".ljust(8), f"{mean_reward_llp:.4f}  {std_reward_llp:.4f}")
+print("BM-FA:".ljust(8), f"{mean_reward_sp:.4f}  {std_reward_sp:<7.4f}")
 print(
     "\tBit rate blocking:",
-    (env_llp.episode_bit_rate_requested - env_llp.episode_bit_rate_provisioned)
-    / env_llp.episode_bit_rate_requested,
+    (env_phy_bmfa_df.episode_bit_rate_requested - env_phy_bmfa_df.episode_bit_rate_provisioned)
+    / env_phy_bmfa_df.episode_bit_rate_requested,
 )
 print(
     "\tRequest blocking:",
-    (env_llp.episode_services_processed - env_llp.episode_services_accepted)
-    / env_llp.episode_services_processed,
+    (env_phy_bmfa_df.episode_services_processed - env_phy_bmfa_df.episode_services_accepted)
+    / env_phy_bmfa_df.episode_services_processed,
+)
+
+
+env_phy_bmfa_rss_df = gym.make("PhyRMSA-v0", **env_args)
+mean_reward_sp, std_reward_sp = evaluate_heuristic(
+    env_phy_bmfa_rss_df, phy_aware_bmfa_rss_rmsa, n_eval_episodes=episodes
+)
+print("BM-FA-Rss:".ljust(8), f"{mean_reward_sp:.4f}  {std_reward_sp:<7.4f}")
+print(
+    "\tBit rate blocking:",
+    (env_phy_bmfa_rss_df.episode_bit_rate_requested - env_phy_bmfa_rss_df.episode_bit_rate_provisioned)
+    / env_phy_bmfa_rss_df.episode_bit_rate_requested,
+)
+print(
+    "\tRequest blocking:",
+    (env_phy_bmfa_rss_df.episode_services_processed - env_phy_bmfa_rss_df.episode_services_accepted)
+    / env_phy_bmfa_rss_df.episode_services_processed,
 )
