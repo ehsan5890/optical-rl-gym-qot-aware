ab6049cb3964834cadf6b4ba4a28ab0a44d7dd75
ehsan5890 <ee.etezadi@gmail.com>
2024-07-05 12:25:23 +0200

    ICTON paper

--- examples/load_inputs.py
+++ examples/load_inputs.py
@@ -4,8 +4,11 @@ from scipy.io import loadmat
 mat_file = loadmat('inputs/Modulation_connection_JPN12_k7SP_CHBFullyLoaded_SCL_Uniform.mat')
 mat_file1 =  loadmat('inputs/GSNR_connection_JPN12_k7SP_CHBFullyLoaded_SCL_Uniform.mat')
 mat_file3 =  loadmat('inputs/All_connections_Profile_JPN12_k7SP_CHBFullyLoaded_SCL_Uniform.mat')
+mat_file4 =  loadmat('inputs/Results_K3SP_FRP_SLC_CBG_USB14.mat')
+mat_file5 =  loadmat('inputs/Results_K3SP_FRP_SLC_CBG_JPN12.mat')
+mat_file6 =  loadmat('inputs/Results_K3SP_FRP_SLC_CBG_SPN30.mat')
 # Access variables from the loaded mat file
-variable_name = mat_file['variable_name']
+# variable_name = mat_file['variable_name']
 
 # Do something with the variable
-print(variable_name)
+print(2)
